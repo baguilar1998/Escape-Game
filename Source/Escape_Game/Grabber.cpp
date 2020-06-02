4ee3d5c8bc25ecf0c352b5139be674f1bceca946
Brian Kenji Aguilar <anjacular@gmail.com>
2020-06-02 14:04:44 -0400

    Changed game mode from c++ to bp and working on grabber component

---
+++ Source/Escape_Game/Grabber.cpp
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "DrawDebugHelpers.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+#include "Grabber.h"
+
+#define OUT
+
+// Sets default values for this component's properties
+UGrabber::UGrabber()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrabber::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	UE_LOG(LogTemp, Warning, TEXT("Grabber Comp Working"));
+}
+
+
+// Called every frame
+void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// Get the player's viewpoint
+	FVector PlayerViewPointLocation;
+	FRotator PlayerViewPointRotation;
+
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(
+		OUT PlayerViewPointLocation,
+		OUT PlayerViewPointRotation
+	);
+
+	// Logging out to test 
+	// FString Location = PlayerViewPointLocation.ToString();
+	// FString Rotation = PlayerViewPointRotation.ToString();
+	// UE_LOG(LogTemp, Warning, TEXT("Location: %s"), *Location);
+	// UE_LOG(LogTemp, Warning, TEXT("Rotation: %s"), *Rotation);
+
+	// Draw a line from player showing the reach
+	FVector LineTraceEnd = PlayerViewPointLocation + PlayerViewPointRotation.Vector() * Reach;
+
+	DrawDebugLine(
+		GetWorld(),
+		PlayerViewPointLocation,
+		LineTraceEnd,
+		FColor(0,255,0),
+		false,
+		0.f,
+		0,
+		5.f
+	);
+
+	// Ray-Cast out to a certain distance (Reach)
+
+	// See what it hits
+}
+
